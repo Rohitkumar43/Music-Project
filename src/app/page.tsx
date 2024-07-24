e83c24de7b71ccb9e0d2b983ca9b715b561e7dcb
rohit kumar <kumarrohit9337@gmail.com>
2024-07-24 09:47:09 +0530

    Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/Herosection";
+import Feauturedsection from '@/components/Feauturedsection';
 
 
 export default function Home() {
   return(
     <main className="min-h-screen bg-black/[0.65] antialiased bg-grid-white/[0.02]">
       <Herosection/>
+      <Feauturedsection/>
     </main>
   )
 }
