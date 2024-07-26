3a15318d2a35bc0961197ba2146bcf154c1a0a7d
rohit kumar <kumarrohit9337@gmail.com>
2024-07-26 18:06:15 +0530

    Done

--- src/components/Feauturedsection.tsx
+++ src/components/Feauturedsection.tsx
@@ -1,12 +1,26 @@
+'use client'
 import React from 'react'
 import Link from 'next/link';
 
 import Coursedata from '../data/music_module.json';
+import {HoverEffect}  from './ui/card-hover-effect';
+import { title } from 'process';
 
+
+interface Courses {
+    id: number,
+    title: String,
+    slug: String,
+    description: String,
+    price: number,
+    instructor: String,
+    isFeatured: boolean,
+    image: String
+}
 
 
 function Feauturedsection(){
-    Coursedata.courses.filter(course => course.isFeatured)
+    const featuredCourses = Coursedata.courses.filter((course:Courses) => course.isFeatured);
     return(
         <div className='py-12 bg-gray-900 mt-20'>
             <div>
@@ -19,7 +33,21 @@ function Feauturedsection(){
             </div>
             <div className='mt-10'>
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
-            gap-8 justify-center'></div>
+            gap-8 justify-center'>
+                {featuredCourses.map((course:Courses) => (
+                    <div key={course.id} className='flex justify-center'>
+                       <HoverEffect className='flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 
+                       overflow-hidden h-full max-w-sm'>
+                        <div className='p-4 sm:p-6 flex flex-col items-center
+                        text-center flex-grow'>
+                            <p>{course.title}</p>
+                            <p>{course.image}</p>
+                            <p>{course.description}</p>
+                        </div>
+                       </HoverEffect>
+                    </div>
+                ))}
+            </div>
             </div>
             <div className='mt-20 text-center'>
                 <Link href={"/courses"} className='px-4 py-2 rounded border border-neutral-600
