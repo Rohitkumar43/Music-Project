2a35aa9e96009301b48f0fdc82aab1658ac15b6a
rohit kumar <kumarrohit9337@gmail.com>
2024-10-03 12:22:03 +0530

    Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Herosection from "@/components/Herosection";
 import Feauturedsection from '@/components/Feauturedsection';
+import Whyus from '@/components/whyus'
 
 // this is the page where is the gloal layout is there 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.65] antialiased bg-grid-white/[0.02]">
       <Herosection/>
       <Feauturedsection/>
+      <Whyus/>
     </main>
   )
 }
